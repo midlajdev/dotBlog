5c58ee2fdd68bf86814122957255567eff06319a
Midlaj <midlajcv810@gmail.com>
2025-10-31 21:48:37 +0530

    postgresql-db added

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 from decouple import config
-
+import dj_database_url
+import os
 
 
 from pathlib import Path
@@ -84,10 +85,9 @@ WSGI_APPLICATION = 'myBlog.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+   )
 }
 
 
