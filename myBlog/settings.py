ec1a9f1fe95e32d0d48eded50a03f0c3cd66e275
Midlaj <midlajcv810@gmail.com>
2025-11-01 08:44:42 +0530

    Configured for Railway deployment

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -85,31 +85,12 @@ WSGI_APPLICATION = 'myBlog.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+#https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL')
-#    )
-# }
+DATABASES = {
+   'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
-
-
-
-
-if DEBUG:
-    # Use SQLite locally
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    # Use Render’s Postgres in production
-    DATABASES = {
-        'default': dj_database_url.config(default=config('DATABASE_URL'))
-    }
 
 
 # Password validation
